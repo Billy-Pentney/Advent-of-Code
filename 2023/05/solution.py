f210f66e22c344d10b42b56379a047d1f7a8d5db
billy.pentney@outlook.com <billy.pentney@outlook.com>
2023-12-10 17:34:16 +0000

    Add Default Solution and start Day 5

---
+++ 2023/05/solution.py
@@ -0,0 +1,37 @@
+import sys, os
+import re
+
+
+def part_one(fileaddr):
+    ## Solve Part One
+    return
+
+def part_two(fileaddr):
+    ## Solve Part Two
+    return
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    filename = args[0]
+    part = args[1]
+    fileaddr = os.path.dirname(os.path.realpath(sys.argv[0])) + "\\" + args[0]
+
+    if os.path.exists(fileaddr):
+        if (part == '1'):
+            result = part_one(fileaddr)
+        else:
+            result = part_two(fileaddr)
+        print("Result:",result)
+    else:
+        print(f"Could not find file at location {fileaddr}")
+    
