354a0f9a5a421ae9f9b094f4ecda28ac3db51b50
billy.pentney@outlook.com <billy.pentney@outlook.com>
2023-12-17 18:05:33 +0000

    Attempt continued

--- 2023/17/solution.py
+++ 2023/17/solution.py
@@ -24,6 +24,7 @@ NORTH = 0
 EAST = 1
 SOUTH = 2
 WEST = 3
+DIRS = [NORTH, EAST, SOUTH, WEST]
 
 
 def get_enterable_neighbours(lines, curr, last_dir, last_dir_count):
@@ -31,16 +32,16 @@ def get_enterable_neighbours(lines, curr, last_dir, last_dir_count):
     (row, col) = curr
 
     # Move North from current
-    if row > 0 and (last_dir_count < 3 or last_dir != NORTH):
+    if row > 0 and (last_dir_count < 3 or last_dir != NORTH) and last_dir != SOUTH:
         nbs.append(((row-1, col), NORTH))
     # Move East from current
-    if col < len(lines[row])-1 and (last_dir_count < 3 or last_dir != EAST):
+    if col < len(lines[row])-1 and (last_dir_count < 3 or last_dir != EAST) and last_dir != WEST:
         nbs.append(((row, col+1), EAST))
     # Move South from current
-    if row < len(lines)-1 and (last_dir_count < 3 or last_dir != SOUTH):
+    if row < len(lines)-1 and (last_dir_count < 3 or last_dir != SOUTH) and last_dir != NORTH:
         nbs.append(((row+1, col), SOUTH))
     # Move West from current
-    if col > 0 and (last_dir_count < 3 or last_dir != WEST):
+    if col > 0 and (last_dir_count < 3 or last_dir != WEST) and last_dir != EAST:
         nbs.append(((row, col-1), WEST))
     
     return nbs
@@ -77,7 +78,7 @@ class State:
         return State(next_cell, dir, self)
 
     def __repr__(self):
-        return f"(Path: ..{self.path[-1]}, last_move: {self.last_move})"
+        return f"(Path: ..{self.path[-2:]}, last_move: {last_move_to_symbol(self.last_move)})"
 
 
 # Calculate square of L2-Norm (square of Pythagorean Distance)
@@ -87,15 +88,19 @@ def calc_direct_distance(pos1, pos2):
 
 def last_move_to_symbol(dir):
     if dir == EAST:
-        return '-'
+        return '>'
     elif dir == NORTH:
-        return '|'
+        return '^'
     elif dir == SOUTH:
-        return '|'
+        return 'v'
     elif dir == WEST:
-        return '-'
+        return '<'
     else:
         return 'X'
+
+
+INF = 100000000
+
 
 ## Solve Part One
 def part_one(fileaddr):
@@ -108,11 +113,12 @@ def part_one(fileaddr):
 
     # min_cost[r][c][d] is the minimum heat-loss in any path from the start
     # to cell at row r, column c, entering via direction d (0-3)
-    min_cost = [[[0,0,0,0] for x in line] for line in lines]
-    moves_used = [[[0,0,0,0] for x in line] for line in lines]
+    min_cost = [[[[INF,INF,INF,INF] for dir in DIRS] for x in line] for line in lines]
+    moves_used = [[[INF,INF,INF,INF] for x in line] for line in lines]
 
     frontier = PriorityQueue()
     frontier.put((calc_direct_distance(source, dest), 0, State(source, None, None)))
+    min_cost[source[0]][source[1]] = [[0,0,0,0] for dir in DIRS]
 
     seen = set()
     iters = 0
@@ -124,7 +130,7 @@ def part_one(fileaddr):
         curr = curr_state.endpoint()
 
         if curr_state.last_move is not None:
-            curr_cost = min_cost[curr[0]][curr[1]][curr_state.last_move]
+            curr_cost = min_cost[curr[0]][curr[1]][curr_state.last_move][curr_state.last_move_count]
             curr_moves = moves_used[curr[0]][curr[1]][curr_state.last_move]
         else:
             curr_cost = 0
@@ -132,24 +138,27 @@ def part_one(fileaddr):
 
         seen.add(curr)
 
-        for nb, dir in get_enterable_neighbours(lines, curr, curr_state.last_move, curr_state.last_move_count):
-            # Compare the heat-loss required to reach nb, entering via direction dir
-            old_cost = min_cost[nb[0]][nb[1]][dir]
-            new_cost = curr_cost + lines[nb[0]][nb[1]]
+        ## NEED TO CONSIDER ALL Directions when setting MinCost
 
+        for nb, dir in get_enterable_neighbours(lines, curr, curr_state.last_move, curr_state.last_move_count):
             new_state = curr_state.gen_move_to(nb, dir)
-            priority = new_cost + calc_direct_distance(nb, dest) + curr_state.last_move_count
 
             # Compare the number of consecutive moves (in dir) required to reach nb
             old_moves = moves_used[nb[0]][nb[1]][dir]
             new_moves = curr_state.last_move_count
-            
-            print(f"Considering {new_state} with priority {priority}")
 
-            if nb not in seen or old_cost == 0 or new_cost < old_cost or new_moves < old_moves:
+            # Compare the heat-loss required to reach nb, entering via direction dir
+            old_cost = min(min_cost[nb[0]][nb[1]][dir][new_moves-1:])
+            new_cost = curr_cost + lines[nb[0]][nb[1]]
+
+            priority = new_cost# + new_moves
+            
+            print(f"Considering {new_state} with cost: {new_cost} vs old_cost: {old_cost}")
+
+            if nb not in seen or old_cost == 0 or new_cost < old_cost:
                 iters += 1
                 prev_state[(nb,dir)] = curr_state
-                min_cost[nb[0]][nb[1]][dir] = new_cost
+                min_cost[nb[0]][nb[1]][dir][new_moves] = new_cost
                 moves_used[nb[0]][nb[1]][dir] = new_moves
                 frontier.put((priority, iters, new_state))
                 seen.add(nb)
@@ -157,25 +166,31 @@ def part_one(fileaddr):
     print_grid(min_cost)
 
     cost_to_goal = min_cost[dest[0]][dest[1]]
-    dir = 0
-    for d in [NORTH, EAST, SOUTH, WEST]:
-        if cost_to_goal[dir] == 0 or (cost_to_goal[d] > 0 and cost_to_goal[d] < cost_to_goal[dir]):
-            dir = d
+    dir = -1
+    min_cost = INF
+    for d in DIRS:
+        if max(cost_to_goal[d]) > 0:
+            goal_cost = min([c for c in cost_to_goal[d] if c > 0])
+            if dir < 0 or goal_cost < min_cost:
+                dir = d
+                min_cost = goal_cost
 
-    # Generate path from the visited cells
-    # path = []
-    # curr_state = State(dest, None, None)
-    # while curr_state.cell != source:
-    #     curr = curr_state.cell
-    #     path.append(curr)
-    #     lines[curr[0]][curr[1]] = last_move_to_symbol(curr_state.last_move)
-    #     curr_state = prev_state[curr]
     path = prev_state[(dest,dir)].path
     for cell in path:
         lines[cell[0]][cell[1]] = 'X'
 
     print_grid(lines, as_string=True)
     return cost_to_goal[dir]
+
+
+
+
+
+
+
+
+
+
 
 
 ## Solve Part Two
