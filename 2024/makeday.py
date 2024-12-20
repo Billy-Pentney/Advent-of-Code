69d260f5756b9faab582103d40b83bc372373fbd
billy.pentney@outlook.com <billy.pentney@outlook.com>
2024-12-20 10:53:46 +0000

    Update makeday to create default python loader code

--- 2024/makeday.py
+++ 2024/makeday.py
@@ -4,6 +4,27 @@ from sys import argv
 from os import mkdir
 from os.path import exists
 
+DEFAULT_PYTHON_SCRIPT = \
+"""
+import numpy as np
+
+
+def load(fname):
+    with open(fname, "r") as file:
+        grid = [list(row.strip("\\n")) for row in file.readlines()]
+
+    return np.array(grid)
+
+
+
+from sys import argv
+
+if __name__ == '__main__':
+    fname = argv[1]
+"""
+
+
+
 if __name__=='__main__':
 
     if len(argv) < 2:
@@ -22,9 +43,14 @@ if __name__=='__main__':
         if not exists(dir_name):
             mkdir(dir_name)
 
-        fnames = [f'{dir_name}.py','test.txt','example.txt']
+        fnames = ['test.txt','example.txt']
         for fname in fnames:
             with open(f'{dir_name}/{fname}', "w") as file:
                 file.write("")
+
+
+        py_name = f'{dir_name}.py'
+        with open(f"{dir_name}/{py_name}", "w") as file:
+            file.write(DEFAULT_PYTHON_SCRIPT)
         
         print(f"Done {dir_name}!")
