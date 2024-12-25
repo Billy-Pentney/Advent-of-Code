2c780a83b1e46f6460bc69285a9a1fdf2e701e2d
billy.pentney@outlook.com <billy.pentney@outlook.com>
2024-12-25 16:22:29 +0000

    Solve Day 3 Part 2

--- 2015/03/solution.py
+++ 2015/03/solution.py
@@ -33,9 +33,43 @@ def part_one(fileaddr):
     return len(visited)
 
 
+def move(x,y,d):
+    if d == '^':
+        return x,y-1
+    elif d == '>':
+        return x+1,y
+    elif d == '<':
+        return x-1,y
+    elif d == 'v':
+        return x,y+1
+
+
 ## Solve Part Two
 def part_two(fileaddr):
-    return
+    line = read_file(fileaddr)
+
+    visited1 = set()
+    visited2 = set()
+
+    x1,y1 = 0,0
+    x2,y2 = 0,0
+
+    for i in range((len(line)+1)//2):
+        d1 = line[2*i]
+        visited1.add((x1,y1))
+        x1,y1 = move(x1,y1,d1)
+
+        if 2*i+1 < len(line):
+            d2 = line[2*i+1]
+            visited2.add((x2,y2))
+            x2,y2 = move(x2,y2,d2)
+
+    visited1.add((x1,y1))
+    visited2.add((x2,y2))
+
+    visited = visited1.union(visited2)
+    # print(visited)
+    return len(visited)
 
 
 
@@ -58,4 +92,7 @@ if __name__ == '__main__':
 
     unique_houses = part_one(fileaddr)
     print(f"(Part 1) Num unique houses: {unique_houses}")
+    
+    unique_houses_two = part_two(fileaddr)
+    print(f"(Part 2) Num unique houses with two santas: {unique_houses_two}")
     
