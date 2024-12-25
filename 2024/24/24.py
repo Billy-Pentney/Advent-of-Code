24f0fa33007babdf9c67e60340087476ab9c3f12
billy.pentney@outlook.com <billy.pentney@outlook.com>
2024-12-25 10:48:36 +0000

    Begin work on Part 2

--- 2024/24/24.py
+++ 2024/24/24.py
@@ -66,10 +66,82 @@ def evaluate_z(inits, ops):
         if var.startswith('z') and val == 1:
             var_num = int(var[1:])
             value = int(pow(2,var_num))
-            print(f"z_{var_num} = {value}")
+            # print(f"z_{var_num} = {value}")
             z_value += value
         
     return z_value
+
+
+
+def decomp(result_val, op):
+    if op == 'XOR':
+        if result_val == 0:
+            return [(0,0),(1,1)]
+        else:
+            return [(1,0),(0,1)]
+    elif op == 'OR':
+        if result_val == 0:
+            return [(0,0)]
+        else:
+            return [(1,0),(0,1),(1,1)]
+    elif op == 'AND':
+        if result_val == 0:
+            return [(0,0),(0,1),(1,0)]
+        else:
+            return [(1,1)]
+    else:
+        print(f"Invalid op {op}")
+        return []
+
+
+
+
+def build_dependency_graph(ops):
+    """
+        Returns a dictionary which maps each variable X to the set of variables which are required to calculate the value of X.
+    """
+    dependencies = {}
+
+    for first,op,second,result in ops:
+        if result not in dependencies.keys():
+            dependencies[result] = {}
+        dependencies[result] = dependencies[result].union([first, second])
+    
+    return dependencies
+
+
+def part_two(inits, ops):
+
+    xs = [var for var in inits.keys() if var.startswith('x')]
+    ys = [var for var in inits.keys() if var.startswith('y')]
+
+    k = max(len(xs), len(ys))
+
+    state = inits.copy()
+
+    for x in range(pow(2,k)):
+        ## Initialise variables x##
+
+        for y in range(pow(2,k)):
+            ## Initialise variables y##
+
+
+
+            expected = x+y
+            result = evaluate_z(state, ops)
+
+
+
+
+
+
+
+
+
+
+
+    swapped_wires = [] ## TODO
+    return ",".join(sorted(swapped_wires))
 
 
 
