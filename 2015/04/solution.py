e5e916a6a84d971fd4a98a237b0b63991e7459e8
billy.pentney@outlook.com <billy.pentney@outlook.com>
2024-12-26 12:49:32 +0000

    Solve Day 4 Part 2

--- 2015/04/solution.py
+++ 2015/04/solution.py
@@ -2,6 +2,8 @@ import sys, os
 import re
 import hashlib
 
+MAX_ITER = 10000000
+
 
 def read_file(fileaddr):
     lines = []
@@ -10,34 +12,35 @@ def read_file(fileaddr):
     return lines[0].strip("\n")
 
 
+def find_md5_with_zero_prefix(base, n_zeros=5, max_iter=MAX_ITER):
+    prefix = '0'*n_zeros
 
-
-## Solve Part One
-def part_one(fileaddr, max_iter=1000000):
-    line = read_file(fileaddr)
-
-    
     for i in range(1, max_iter):
-        input_str = line + str(i)
+        input_str = base + str(i)
         hash = hashlib.md5(input_str.encode())
 
         hex_hash = hash.hexdigest()
-        if hex_hash.startswith('00000'):
-            print("Found MD5 with 5-zero prefix...")
+        if hex_hash.startswith(prefix):
+            print(f"Found MD5 with {n_zeros}-zero prefix...")
             print(f" >> MD5({input_str}) = {hex_hash}")
 
             return i
         
+    print(f"No solutions in first {max_iter} iterations!")
     return None
 
 
+
+## Solve Part One
+def part_one(fileaddr, max_iter=1000000):
+    line = read_file(fileaddr)  
+    return find_md5_with_zero_prefix(line, 5, max_iter)
+
+
 ## Solve Part Two
-def part_two(fileaddr):
-    return
-
-
-
-
+def part_two(fileaddr, max_iter=MAX_ITER):
+    line = read_file(fileaddr)
+    return find_md5_with_zero_prefix(line, 6, max_iter)
 
 
 
@@ -57,5 +60,5 @@ if __name__ == '__main__':
     part_one_ans = part_one(fileaddr)
     print(f"(Part 1) Solution: {part_one_ans}")
     
-    # part_two_ans = part_two(fileaddr)
-    # print(f"(Part 2) Solution: {part_two_ans}")
+    part_two_ans = part_two(fileaddr)
+    print(f"(Part 2) Solution: {part_two_ans}")
