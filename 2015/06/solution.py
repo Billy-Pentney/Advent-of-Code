02ea3b18c74f936ec1b1171e92e36b79a97db3fa
billy.pentney@outlook.com <billy.pentney@outlook.com>
2024-12-26 20:10:36 +0000

    Solve Day 6 Part 2

--- 2015/06/solution.py
+++ 2015/06/solution.py
@@ -59,7 +59,26 @@ def part_one(fileaddr):
 
 ## Solve Part Two
 def part_two(fileaddr):
-    return
+    actions = read_file(fileaddr)
+    grid = np.zeros((1000,1000))
+
+    for action in actions:
+        print(action)
+        instruction = action['instruction']
+        x1,y1 = action['start']
+        x2,y2 = action['end']
+
+        x2 = x2+1
+        y2 = y2+1
+
+        if instruction == 'off':
+            grid[x1:x2,y1:y2] = (grid[x1:x2,y1:y2]-1).clip(0)
+        elif instruction == 'on':
+            grid[x1:x2,y1:y2] += 1
+        elif instruction == 'flip':
+            grid[x1:x2,y1:y2] += 2
+
+    return grid.sum()
 
 
 
@@ -73,15 +92,16 @@ def part_two(fileaddr):
 if __name__ == '__main__':
     args = sys.argv[1:]
     filename = args[0]
-    # part = args[1]
+    part = int(args[1])
     fileaddr = os.path.dirname(os.path.realpath(sys.argv[0])) + "\\" + args[0]
 
     if not os.path.exists(fileaddr):
         print(f"Could not find file at location {fileaddr}")
         exit(1)
 
-    part_one_ans = part_one(fileaddr)
-    print(f"(Part 1) Solution: {part_one_ans}")
-    
-    # part_two_ans = part_two(fileaddr)
-    # print(f"(Part 2) Solution: {part_two_ans}")
+    if part == 1:
+        part_one_ans = part_one(fileaddr)
+        print(f"(Part 1) Solution: {part_one_ans}")
+    elif part == 2:
+        part_two_ans = part_two(fileaddr)
+        print(f"(Part 2) Solution: {part_two_ans}")
