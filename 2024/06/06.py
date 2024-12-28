910c8bb5d9a36a5588aaa0785c50b2ed542ccbe8
billy.pentney@outlook.com <billy.pentney@outlook.com>
2024-12-28 17:53:48 +0000

    Solve Day 6 Part 2

--- 2024/06/06.py
+++ 2024/06/06.py
@@ -10,10 +10,10 @@ def load(fname):
 
 
 
-NORTH = [-1,0]
-SOUTH = [1,0]
-EAST = [0,1]
-WEST = [0,-1]
+NORTH = [0,-1]
+SOUTH = [0,1]
+EAST = [1,0]
+WEST = [-1,0]
 
 dirs = [NORTH, EAST, SOUTH, WEST]
 
@@ -26,34 +26,77 @@ OBSTACLE = '#'
 BLOCKER = 'O'
 VISITED = 'X'
 
-def part_one(grid):
+
+def check_for_loop(grid, state):
+    """
+        Checks if the movement of the guard from the given state leads to an infinite loop.
+        State is a 3-tuple (x,y,d) where (x,y) is the coordinates in the grid and d is the direction (0=NORTH,1=EAST,2=SOUTH,3=WEST).
+        Returns true if a loop is found.
+    """
+    x,y,d = state
+    
+    rows = len(grid)
+    cols = len(grid[0])
+    visited = np.zeros((rows,cols,4), dtype='int')
+
+    while x >= 0 and y >= 0 and x < cols and y < rows:
+        if visited[x,y,d] == 1:
+            return True
+        
+        visited[x,y,d] = 1
+
+        vec = dirs[d]
+        nx,ny = x+vec[0],y+vec[1]
+
+        if nx >= 0 and ny >= 0 and nx < cols and ny < rows:
+            if grid[ny][nx] == OBSTACLE or grid[ny][nx] == BLOCKER:
+                # Turn right 90 degrees
+                d = (d+1)%4
+            else:
+                x,y = nx,ny
+                # Move and keep same direction
+        else:
+            ## About to leave the map
+            # print(f" > Leaving map at {(nx,ny)}")
+            return False
+
+    # print(f" > Left map at {(x,y)}")
+    return False
+
+
+
+
+
+
+def solve_both_parts(grid):
     rows = len(grid)
     cols = len(grid[0])
 
-    X,Y,d = 0,0,0
+    cx,cy,d = 0,0,0
     seen = np.zeros((rows, cols, 4))
 
+    # Find the starting position and direction
     for y in range(rows):
         for x in range(cols):
             if grid[y][x] == UP:
-                X,Y,d = x,y,0
+                cx,cy,d = x,y,0
             elif grid[y][x] == RIGHT:
-                X,Y,d = x,y,1
+                cx,cy,d = x,y,1
             elif grid[y][x] == DOWN:
-                X,Y,d = x,y,2
+                cx,cy,d = x,y,2
             elif grid[y][x] == LEFT:
-                X,Y,d = x,y,d
+                cx,cy,d = x,y,3
 
 
+    ## Indicates if the given cell is a blocker (-1 = not checked, 0 = no, 1 = yes)
+    blockers = np.full((rows,cols), -1, dtype='int')
     n_blockers = 0
 
+    ## Follow the path
     while True:
         vec = dirs[d]
-        nx, ny = X + vec[1], Y + vec[0]
-        seen[Y][X][d] = 1
-
-        if grid[Y][X] != BLOCKER:
-            grid[Y][X] = VISITED
+        nx, ny = cx + vec[0], cy + vec[1]
+        seen[cy][cx][d] = 1
 
         ## If about to leave the map, stop
         if nx < 0 or nx >= cols or ny < 0 or ny >= rows:
@@ -62,63 +105,45 @@ def part_one(grid):
         if grid[ny][nx] == OBSTACLE:
             # Rotate 90 degrees clockwise
             d = (d+1)%4
-            seen[Y][X][d] = 1
+            seen[cy][cx][d] = 1
 
         else:
 
-            ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ PART TWO
+            ## ~~~~~~~~~ PART TWO - Check if this is a blocker ###
 
-            # If there were an obstacle, would it create a cycle?
-            d_ = (d+1)%4
-            v_ = dirs[d_]
-            aX, aY = X+v_[1], Y+v_[0]
+            if blockers[ny][nx] < 0:
+                ## Suppose we place an obstacle at (nx,ny)
+                grid2 = np.array(grid)
+                grid2[ny][nx] = BLOCKER
+                grid2[cy][cx] = 'M'
+ 
+                # print(f"Checking for blocker at... ({nx},{ny})")
+                # If there were an obstacle, would it create a cycle?
+                creates_cycle = check_for_loop(grid2, (cx,cy,d))
 
-            ## Make a copy for this exploration, so we don't affect the actual
-            seen_temp = seen.copy()
-
-            ## Does this rejoin an existing path we've taken?
-            ## We check until we leave the map or revisit a cell.
-            while aX >= 0 and aY >= 0 and aX < cols and aY < rows:
-                if seen_temp[aY][aX][d_] > 0:
-                    ## Already visited this cell from this direction
+                if creates_cycle:
+                    blockers[ny][nx] = 1
                     n_blockers += 1
-                    print(f"Blocker at {nx},{ny}")
-                    grid[ny][nx] = BLOCKER
-                    break
-            
-                seen_temp[aY][aX][d_] = 2
-                
-                ## Check if we can proceed to the next cell
-                bX,bY = aX+v_[1], aY+v_[0]
-                if bX >= 0 and bY >= 0 and bX < cols and bY < rows:
-                    if grid[bY][bX] == OBSTACLE:
-                        # Rotate 90
-                        d_ = (d_+1)%4
-                        v_ = dirs[d_]
-
-                    aX += v_[1]
-                    aY += v_[0]
-
+                    ## Don't overwrite the grid here otherwise we impact other cells.
                 else:
-                    break
-
-             
+                    blockers[ny][nx] = 0
 
             ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
             # Step forward
-            X,Y = nx,ny
+            cx,cy = nx,ny
         
-
     visited = (seen == 1).any(axis=2).astype(int)
     n_unique_visited = np.sum(visited, dtype='int')
 
+    # Draw the blockers on the grid
+    for by,bx in np.argwhere(blockers == 1):
+        grid[by][bx] = BLOCKER
 
     for row in grid:
         print("".join(row))
         
     return n_unique_visited, n_blockers
-
 
 
 
@@ -132,7 +157,7 @@ if __name__ == '__main__':
     fname = argv[1]
     grid_map = load(fname)
 
-    n_unique, n_blockers = part_one(grid_map)
+    n_unique, n_blockers = solve_both_parts(grid_map)
     print(f"(Part 1) Num Cells: {n_unique}")
     print(f"(Part 2) Num Blocker positions: {n_blockers}")
 
