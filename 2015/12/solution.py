435978a22c5efef771b28eeae7c8bc501764bed5
billy.pentney@outlook.com <billy.pentney@outlook.com>
2024-12-29 15:22:49 +0000

    Solve Day 12 Part 2

--- 2015/12/solution.py
+++ 2015/12/solution.py
@@ -26,9 +26,43 @@ def part_one(fileaddr):
     return total
 
 
+
+import json
+
+
+def sum_json_nums(json_obj):
+    total = 0
+    # print("Summing", json_obj)
+
+    if isinstance(json_obj, str):
+        return 0
+    elif isinstance(json_obj, bool):
+        return 0
+
+    if isinstance(json_obj, dict):
+        if 'red' in json_obj.values():
+            print(f"Skipping {json_obj} with value 'red'")
+            return 0
+        for attr, val in json_obj.items():
+            total += sum_json_nums(val)
+    elif isinstance(json_obj, list):
+        for obj in json_obj:
+            total += sum_json_nums(obj)
+    elif isinstance(json_obj, int) or isinstance(json_obj, float):
+        total += json_obj
+    
+    return total
+
+
 ## Solve Part Two
 def part_two(fileaddr):
-    return
+    text = read_file(fileaddr)
+
+    json_dict = json.loads(text)
+    print(json_dict)
+    
+    return sum_json_nums(json_dict)
+    # return 0
 
 
 
@@ -52,5 +86,5 @@ if __name__ == '__main__':
     part_one_ans = part_one(fileaddr)
     print(f"(Part 1) Solution: {part_one_ans}")
     
-    # part_two_ans = part_two(fileaddr)
-    # print(f"(Part 2) Solution: {part_two_ans}")
+    part_two_ans = part_two(fileaddr)
+    print(f"(Part 2) Solution: {part_two_ans}")
